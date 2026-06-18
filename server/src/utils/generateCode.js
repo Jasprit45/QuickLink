ad5dc0be69fb462bdb0d66d20496bb1f673b5eb9
Jasprit45 <singhsardarjasprit@gmail.com>
2026-06-18 23:25:29 +0530

    Installed some package

--- server/src/utils/generateCode.js
+++ server/src/utils/generateCode.js
@@ -1,5 +1,5 @@
 const {nanoid} = require('nanoid');
 
-const generateCode = () => nanoid(7); //7 character - 3.5 trillion combinations
+const generateCode = () => nanoid(7); //7 character - 3.5 trillion
 
 module.exports = generateCode;
